0b67312f829c52453293fcca43d4b4e154bffdfc
David <DMcCulloch86@gmail.com>
2019-10-06 21:39:57 -0400

    inital html, css and javascript structure.  temporary look to start to develop game

---
+++ assets/javascript/app.js
@@ -0,0 +1,38 @@
+// To Do:
+// ending screen "title / ending statment / correct, incorrect, unasnwered / start over button"
+
+$(document).ready(function() {
+
+    //set global variables
+    let quiz = {
+        'question-1': {q: '', a: '', a2: '', a3: '', a4: ''},
+        'question-2': {q: '', a: '', a2: '', a3: '', a4: ''},
+        'question-3': {q: '', a: '', a2: '', a3: '', a4: ''},
+        'question-4': {q: '', a: '', a2: '', a3: '', a4: ''},
+        'question-5': {q: '', a: '', a2: '', a3: '', a4: ''},
+        'question-6': {q: '', a: '', a2: '', a3: '', a4: ''},
+        'question-7': {q: '', a: '', a2: '', a3: '', a4: ''},
+        'question-8': {q: '', a: '', a2: '', a3: '', a4: ''},
+
+    }
+
+
+
+
+
+    //set functions
+
+    function questionTimer () {
+        alert('working') //test
+        //$('#time-clock').text('Time Remaining: ' + )
+    };
+
+
+
+
+
+    //start game - call functions
+
+    questionTimer(); //test
+
+});
