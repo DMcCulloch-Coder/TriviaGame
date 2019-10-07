a3dff6047c4e2ed5730ff20911bdb39670d85860
David <DMcCulloch86@gmail.com>
2019-10-06 23:08:02 -0400

    started to add questions to quiz, add some more structure to html and style to css sheet

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,15 +4,23 @@
 $(document).ready(function() {
 
     //set global variables
-    let quiz = {
-        'question-1': {q: '', a: '', a2: '', a3: '', a4: ''},
-        'question-2': {q: '', a: '', a2: '', a3: '', a4: ''},
-        'question-3': {q: '', a: '', a2: '', a3: '', a4: ''},
-        'question-4': {q: '', a: '', a2: '', a3: '', a4: ''},
-        'question-5': {q: '', a: '', a2: '', a3: '', a4: ''},
-        'question-6': {q: '', a: '', a2: '', a3: '', a4: ''},
-        'question-7': {q: '', a: '', a2: '', a3: '', a4: ''},
-        'question-8': {q: '', a: '', a2: '', a3: '', a4: ''},
+    let quiz = { //what is the best way to show which is correct?
+        'question-1': {q: 'What do the Dursley\'s get Harry for Christmas in his first year?', 
+            a: 'Old Socks', a2: 'Tissue', a3: '50 pence piece', a4: 'Toothpick', c: 'a3'},
+        'question-2': {q: 'Which location is NOT in Diagon Alley?', 
+            a: 'Eeylops Owl Emporium', a2: 'Dervish & Bangs', a3: 'Magical Menagerie', a4: 'Twilfitt and Tatting\'s', c: 'a2'},
+        'question-3': {q: '', 
+            a: '', a2: '', a3: '', a4: '', c: ''},
+        'question-4': {q: '', 
+            a: '', a2: '', a3: '', a4: '', c: ''},
+        'question-5': {q: 'What was Harry\'s first pet?', 
+            a: 'Snow Owl', a2: 'Frog', a3: 'Snake', a4: 'Cat', c: 'a4'},
+        'question-6': {q: '', 
+            a: '', a2: '', a3: '', a4: '', c: ''},
+        'question-7': {q: '', 
+            a: '', a2: '', a3: '', a4: '', c: ''},
+        'question-8': {q: '', 
+            a: '', a2: '', a3: '', a4: '', c: ''},
 
     }
 
@@ -32,6 +40,8 @@ $(document).ready(function() {
 
 
     //start game - call functions
+
+    ('.answer').
 
     questionTimer(); //test
 
