a1fb2117ada0fdc021aa054e30d97fded2c252d3
David <DMcCulloch86@gmail.com>
2019-10-10 01:03:28 -0400

    fixed type on image links, fixed notes on code, added empty function that will check for correctness

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,21 +8,21 @@ $(document).ready(function() {
     //set global variables
     let quiz = { 
         'question-1': {q: 'What do the Dursley\'s get Harry for Christmas in his first year?', 
-            a1: 'Old Socks', a2: 'Tissue', a3: '50 pence piece', a4: 'Toothpick', c: '50 pence piece', i:'assets/images/steam.webp'},
+            a1: 'Old Socks', a2: 'Tissue', a3: '50 pence piece', a4: 'Toothpick', c: '50 pence piece', i:'../images/steam.webp'},
         'question-2': {q: 'Which store is NOT in Diagon Alley?', 
-            a1: 'Eeylops Owl Emporium', a2: 'Dervish & Bangs', a3: 'Magical Menagerie', a4: 'Twilfitt & Tatting\'s', c: 'Dervish & Bangs', i:'assets/images/liar.webp'},
+            a1: 'Eeylops Owl Emporium', a2: 'Dervish & Bangs', a3: 'Magical Menagerie', a4: 'Twilfitt & Tatting\'s', c: 'Dervish & Bangs', i:'../images/liar.webp'},
         'question-3': {q: 'What spell made Herminone\'s teeth to rapidly enlarge?', 
-            a1: 'Densaugeo', a2: 'Tarantellegra', a3: 'Episkey', a4: 'Fornunculus', c: 'Densaugeo', i:'assets/images/worth-it.webp'},
+            a1: 'Densaugeo', a2: 'Tarantellegra', a3: 'Episkey', a4: 'Fornunculus', c: 'Densaugeo', i:'../images/worth-it.webp'},
         'question-4': {q: 'What was the first potion Snape makes first year\'s learn?', 
-            a1: 'Polyjuice Potion', a2: 'A potion to cure boils', a3: 'Polyjuice Potion', a4: 'An antidote to common poisons', c: 'A potion to cure boils', i:'assets/images/slap.webp'},
+            a1: 'Polyjuice Potion', a2: 'A potion to cure boils', a3: 'Polyjuice Potion', a4: 'An antidote to common poisons', c: 'A potion to cure boils', i:'../images/slap.webp'},
         'question-5': {q: 'What was Harry\'s first pet?', 
-            a1: 'Snow Owl', a2: 'Frog', a3: 'Snake', a4: 'Cat', c: 'Cat', i:'assets/images/cat.webp'},
+            a1: 'Snow Owl', a2: 'Frog', a3: 'Snake', a4: 'Cat', c: 'Cat', i:'../images/cat.webp'},
         'question-6': {q: 'What is Harry\'s signiture spell?', 
-            a1: 'Expelliarmus', a2: 'Avada Kedavra', a3: 'Hocus Pocus', a4: 'Accio', c: 'Expelliarmus', i:'assets/images/clap.webp'},
+            a1: 'Expelliarmus', a2: 'Avada Kedavra', a3: 'Hocus Pocus', a4: 'Accio', c: 'Expelliarmus', i:'../images/clap.webp'},
         'question-7': {q: 'What does Dumbledore love about Muggle magazines?', 
-            a1: 'Gardening supplements', a2: 'Comic strips', a3: 'Crossword puzzles', a4: 'Knitting patterns', c: 'Knitting patterns', i:'assets/images/vold.webp'},
+            a1: 'Gardening supplements', a2: 'Comic strips', a3: 'Crossword puzzles', a4: 'Knitting patterns', c: 'Knitting patterns', i:'../images/vold.webp'},
         'question-8': {q: 'What did Dobby the House Elf call Ron?', 
-            a1: 'Won-Won', a2: 'Weasel', a3: 'Wheezy', a4: 'Woozy', c: 'Wheezy', i:'assets/images/fire.webp'},
+            a1: 'Won-Won', a2: 'Weasel', a3: 'Wheezy', a4: 'Woozy', c: 'Wheezy', i:'../images/fire.webp'},
 
     }
     
@@ -36,9 +36,16 @@ $(document).ready(function() {
     //set functions
     function timeWaster () {
         time--
+
         $('#time-clock').text(time)
+        
         if (time > 0) {
             countDown = setTimeout(timeWaster,1000)
+
+        } else {
+            timeOut++
+            checkCorrectness();
+
         }
 
     }
@@ -51,7 +58,7 @@ $(document).ready(function() {
 
         $('.question-box').on('click', function() {
             console.log('working');
-            clearTimeout(countDown); //need?
+            clearTimeout(countDown);
         })
 
     };
@@ -67,6 +74,9 @@ $(document).ready(function() {
         
     }
 
+    function checkCorrectness () { //finish - find if right or wrong, change text for intermediary screen
+        alert('checking')
+    }
     
     function start(i) {
         displayQuestion(i);
